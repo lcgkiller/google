54922f68ac9051c0d1260bf3139410ec65f13da3
lcgkiller <lcgkiller@gmail.com>
2017-07-06 16:19:58 +0900

    settings파일 초기화

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -16,6 +16,9 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_DIR = os.path.dirname(BASE_DIR)
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+print(TEMPLATES_DIR)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
